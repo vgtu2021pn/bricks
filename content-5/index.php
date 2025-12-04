5b617ca148a596417c6e01779d95c141a72be45a
Contributor Shephard <80837156+vgtu2021pn@users.noreply.github.com>
2025-12-04 16:07:31 +0200

    Update index.php

--- content-5/index.php
+++ content-5/index.php
@@ -19,8 +19,8 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
   <title>Bricks Content Page #5</title>  
   <!-- Included CSS Files (Uncompressed) -->
   <!--
@@ -30,10 +30,6 @@
   <link rel="stylesheet" href="../stylesheets/foundation.min.css">
   <link rel="stylesheet" href="../stylesheets/app.css">
   <script src="../javascripts/modernizr.foundation.js"></script>
-  <!-- IE Fix for HTML5 Tags -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
 </head>
 <body>
 <div class="row">
@@ -133,3 +129,4 @@
   
 </body>
 </html>
+
