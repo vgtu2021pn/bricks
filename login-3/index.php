395e3ab67cd68f13152dbb6ce5f0886f68b5caf3
Contributor Shephard <80837156+vgtu2021pn@users.noreply.github.com>
2025-12-04 16:11:01 +0200

    Update index.php

--- login-3/index.php
+++ login-3/index.php
@@ -17,8 +17,8 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
   <title>Bricks Login Form #3</title>
   <!-- Included CSS Files (Uncompressed) -->
   <!--
@@ -29,11 +29,8 @@
   <link rel="stylesheet" href="../stylesheets/app.css">
   <script src="../javascripts/modernizr.foundation.js"></script>
   <link rel="icon" href="../favicon.ico" type="image/x-icon">
-  <!-- IE Fix for HTML5 Tags -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'></head>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+</head>
 <body>
 <div class="row">
 	<div class="four columns centered">
@@ -83,3 +80,4 @@
   <script src="../javascripts/app.js"></script>
 </body>
 </html>
+
